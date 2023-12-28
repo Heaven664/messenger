86339d3644d2e111ec73e901014e92f423b8b201
Omar Hamid <omarhamid664@gmail.com>
2023-12-28 01:30:20 -0700

    Finish background profile

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import styles from "@/components/Profile/Profile.module.scss";
 import Image from "next/image";
-import { MoreVert } from "@mui/icons-material/";
+import { MoreVert, Logout } from "@mui/icons-material/";
 import { Menu, MenuItem } from "@mui/material/";
 import { useState } from "react";
 
@@ -18,35 +18,46 @@ const Profile = () => {
 
   return (
     <div className={styles.container}>
-      <div>
-        <div className={styles.backgroundContainer}>
-          <Image
-            src={"/general/background-image.jpg"}
-            width={1000}
-            height={1000}
-            priority={true}
-            alt="Profile Background Image"
-          />
-          <h5>My Profile</h5>
-          <div className={styles.moreVertContainer} onClick={handleClick}>
-            <MoreVert />
-          </div>
-          {anchorEl && (
-            <div className={styles.optionsContainer}>
-              <Menu
-                open={Boolean(anchorEl)}
-                anchorEl={anchorEl}
-                onClose={() => setAnchorEl(null)}
-              >
-                <ul>
-                  <MenuItem onClick={handleLogOut} key={"logout"}>
-                    Log out
-                  </MenuItem>
-                </ul>
-              </Menu>
-            </div>
-          )}
+      <div className={styles.backgroundContainer}>
+        <Image
+          src={"/general/background-image.jpg"}
+          width={1000}
+          height={1000}
+          priority={true}
+          alt="Profile Background Image"
+        />
+        <h5>My Profile</h5>
+        <div className={styles.moreVertContainer} onClick={handleClick}>
+          <MoreVert />
         </div>
+        {anchorEl && (
+          <div className={styles.optionsContainer}>
+            <Menu
+              open={Boolean(anchorEl)}
+              anchorEl={anchorEl}
+              autoFocus={false}
+              onClose={() => setAnchorEl(null)}
+              anchorOrigin={{
+                vertical: 28,
+                horizontal: -110,
+
+              }}
+            >
+              <MenuItem
+                onClick={handleLogOut}
+                key={"logout"}
+                selected={false}
+                sx={{
+                  display: "flex",
+                  gap: "8px",
+                }}
+              >
+                <p>Log Out</p>
+                <Logout />
+              </MenuItem>
+            </Menu>
+          </div>
+        )}
       </div>
       <div>Profile Hero</div>
       <div>Profile Info</div>
