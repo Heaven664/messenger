82c313b0ce8ac9c866c075ee413471291f5b371b
Omar Hamid <omarhamid664@gmail.com>
2023-12-30 12:38:23 -0700

    Finish ContactActions component

--- components/Contacts/ContactsActions.tsx
+++ components/Contacts/ContactsActions.tsx
@@ -1,5 +1,7 @@
 import AddIcon from "@mui/icons-material/Add";
+import SearchIcon from "@mui/icons-material/Search";
 import styles from "./ContactsActions.module.scss";
+import { TextField } from "@mui/material";
 
 const ContactsActions = () => {
   return (
@@ -7,11 +9,17 @@ const ContactsActions = () => {
       <div className={styles.topSection}>
         <h3>Contacts</h3>
         <button>
-          <AddIcon fontSize="inherit"/>
+          <AddIcon fontSize="inherit" />
         </button>
       </div>
       <div className={styles.bottomSection}>
-        <h3>Search Contacts</h3>
+        <TextField
+          id="contact-search-field"
+          variant="outlined"
+          label="Search Contacts"
+          className={styles.inputField}
+        />
+        <SearchIcon className={styles.searchIcon} fontSize="inherit"/>
       </div>
     </div>
   );
