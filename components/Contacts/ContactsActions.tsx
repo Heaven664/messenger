eeba7d2d00227cbf4584200bf2366df97241c060
Omar Hamid <omarhamid664@gmail.com>
2023-12-30 12:17:21 -0700

    Style contacts action top section

--- components/Contacts/ContactsActions.tsx
+++ components/Contacts/ContactsActions.tsx
@@ -1,3 +1,4 @@
+import AddIcon from "@mui/icons-material/Add";
 import styles from "./ContactsActions.module.scss";
 
 const ContactsActions = () => {
@@ -5,7 +6,9 @@ const ContactsActions = () => {
     <div className={styles.container}>
       <div className={styles.topSection}>
         <h3>Contacts</h3>
-        <button>Add</button>
+        <button>
+          <AddIcon fontSize="inherit"/>
+        </button>
       </div>
       <div className={styles.bottomSection}>
         <h3>Search Contacts</h3>
