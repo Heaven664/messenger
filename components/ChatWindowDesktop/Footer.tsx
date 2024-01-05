ca46b752c144aa1d1bbca71adc0b1e2381ce861f
Omar Hamid <omarhamid664@gmail.com>
2024-01-04 22:31:06 -0700

    Add emoji picker component

--- components/ChatWindowDesktop/Footer.tsx
+++ components/ChatWindowDesktop/Footer.tsx
@@ -1,13 +1,44 @@
 import SentimentSatisfiedOutlinedIcon from "@mui/icons-material/SentimentSatisfiedOutlined";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
 
 import styles from "@/components/ChatWindowDesktop/Footer.module.scss";
+import { useState } from "react";
 
 const ChatWindowDesktopFooter = () => {
+  const [emojiPicker, setEmojiPicker] = useState<Boolean>(true);
+
+  // Close emoji picker when user clicks outside of it
+  const handleClickOutside = (e: any) => {
+    if (e.target.id !== "emojiIcon") {
+      setEmojiPicker(false);
+    }
+  };
+
+  const handleEmojiSelect = (e: any) => {
+    console.log(e.native);
+  };
+
   return (
     <footer className={styles.container}>
       <div className={styles.optionsSection}>
-        <button className={styles.emojiButton}></button>
-        <SentimentSatisfiedOutlinedIcon className={styles.emojiIcon} />
+        <SentimentSatisfiedOutlinedIcon
+          className={styles.emojiIcon}
+          id="emojiIcon"
+          onClick={() => setEmojiPicker((prev) => !prev)}
+        />
+        {emojiPicker && (
+          <div className={styles.emojiPickerContainer}>
+            <Picker
+              data={data}
+              onEmojiSelect={handleEmojiSelect}
+              emojiSize={"22"}
+              previewPosition={"none"}
+              onClickOutside={handleClickOutside}
+              theme={"dark"}
+            />
+          </div>
+        )}
       </div>
       <div className={styles.inputSection}>input</div>
       <div className={styles.buttonSection}>button</div>
