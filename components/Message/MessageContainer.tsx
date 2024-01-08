939b18f58f55d64819f84fa881d1bc9e1c723ad3
Omar Hamid <omarhamid664@gmail.com>
2024-01-08 00:02:13 -0700

    Style both message states

--- components/Message/MessageContainer.tsx
+++ components/Message/MessageContainer.tsx
@@ -3,14 +3,23 @@ import MessageItem from "./MessageItem";
 import { dummyMessages } from "@/dummyMessage";
 
 const MessageContainer = () => {
-
-  const messages = dummyMessages.map((message) => <MessageItem key={message.messageId} message={message} />);
+  const messages = dummyMessages.map((message, index) => {
+    let lastMessage = true;
+    if (index !== dummyMessages.length - 1) {
+      lastMessage = dummyMessages[index + 1].senderId !== message.senderId;
+    }
+    return (
+      <MessageItem
+        key={message.messageId}
+        message={message}
+        lastMessage={lastMessage}
+      />
+    );
+  });
 
   return (
     <div className={styles.container}>
-      <ul className={styles.listContainer}>
-        {messages}
-      </ul>
+      <ul className={styles.listContainer}>{messages}</ul>
     </div>
   );
 };
--- components/Message/MessageItem.tsx
+++ components/Message/MessageItem.tsx
@@ -1,30 +1,78 @@
 import Image from "next/image";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 import styles from "./MessageItem.module.scss";
+import ownerStyles from "./MessageOwner.module.scss";
 import { MessageType } from "@/types/ChatWindow/types";
 import { timestampToLocalTime } from "@/helpers/ChatWindow";
+import { useState } from "react";
 
 type P = {
   message: MessageType;
+  lastMessage: boolean;
 };
 
-const MessageItem = ({ message }: P) => {
+const userId = "1";
+
+const MessageItem = ({ message, lastMessage }: P) => {
+  const isMessageOwner = message.senderId === userId;
+  const [isMessageRead, setIsMessageRead] = useState(message.viewed);
+
   return (
-    <li className={styles.container}>
-      <div className={styles.messageContainer}>
-        <div className={styles.senderPictureContainer}>
-          <Image
-            src={"/general/main.HEIC"}
-            alt="some-alt"
-            width={40}
-            height={40}
-          />
+    <li className={isMessageOwner ? ownerStyles.container : styles.container}>
+      <div
+        className={
+          isMessageOwner
+            ? ownerStyles.messageContainer
+            : styles.messageContainer
+        }
+      >
+        <div
+          className={
+            isMessageOwner
+              ? ownerStyles.senderPictureContainer
+              : styles.senderPictureContainer
+          }
+        >
+          {lastMessage && (
+            <Image
+              src={message.senderImageUrl}
+              alt={message.messageId}
+              width={40}
+              height={40}
+            />
+          )}
         </div>
-        <div className={styles.messageContainer}>
-          <div className={styles.messageBody}>
+        <div
+          className={
+            isMessageOwner
+              ? ownerStyles.messageContainer
+              : styles.messageContainer
+          }
+        >
+          <div
+            className={
+              isMessageOwner ? ownerStyles.messageBody : styles.messageBody
+            }
+          >
             <p>{message.messageBody}</p>
           </div>
-          <div className={styles.messageInfo}>
-            <p>{timestampToLocalTime(message.sentTime)}</p>
+          <div
+            className={
+              isMessageOwner ? ownerStyles.messageInfo : styles.messageInfo
+            }
+          >
+            {isMessageOwner && lastMessage && (
+              <div
+                className={
+                  isMessageRead
+                    ? ownerStyles.readCheckIcon
+                    : ownerStyles.checkIcon
+                }
+              >
+                <DoneAllIcon fontSize="inherit" color="inherit" />
+              </div>
+            )}
+            {lastMessage && <p>{timestampToLocalTime(message.sentTime)}</p>}
           </div>
         </div>
       </div>
