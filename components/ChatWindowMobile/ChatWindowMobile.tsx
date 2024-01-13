7df91e3c1502365f8629315fc169a1bc0094d3c1
Omar Hamid <omarhamid664@gmail.com>
2024-01-13 09:52:39 -0700

    Create ChatWindowMobile component

---
+++ components/ChatWindowMobile/ChatWindowMobile.tsx
@@ -0,0 +1,11 @@
+import styles from "./ChatWindowMobile.module.scss"
+
+const ChatWindowMobile = () => {
+  return (
+    <div>
+      <h1>Chat Mobile</h1>
+    </div>
+  );
+};
+
+export default ChatWindowMobile;
