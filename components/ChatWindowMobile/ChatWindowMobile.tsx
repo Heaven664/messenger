7d469cf3df2c94fa82879bbb0cf47880720b0b90
Omar Hamid <omarhamid664@gmail.com>
2024-01-13 10:39:32 -0700

    Fix ChatWindowMobile css

--- components/ChatWindowMobile/ChatWindowMobile.tsx
+++ components/ChatWindowMobile/ChatWindowMobile.tsx
@@ -1,9 +1,32 @@
-import styles from "./ChatWindowMobile.module.scss"
+import { useContext, useState } from "react";
+import styles from "./ChatWindowMobile.module.scss";
+import ChatWindowContext from "@/context/ChatWindowContext";
+import { MessageType } from "@/types/ChatWindow/types";
+import { dummyMessages } from "@/dummyMessage";
+import ChatWindowDesktopHeader from "../ChatWindowDesktop/Header";
+import MessageContainer from "../Message/MessageContainer";
+import Footer from "../ChatWindowDesktop/Footer";
+import EmptyChat from "../ChatWindowDesktop/EmptyChatWindow";
 
 const ChatWindowMobile = () => {
+  const chatWindowDesktopContext = useContext(ChatWindowContext);
+  const chatWindowSelected = chatWindowDesktopContext?.headerInfo !== null;
+  const [messages, setMessages] = useState<MessageType[]>(dummyMessages);
+  const addMessage = (message: MessageType) => {
+    setMessages([...messages, message]);
+  };
+
   return (
-    <div>
-      <h1>Chat Mobile</h1>
+    <div className={styles.container}>
+      {chatWindowSelected ? (
+        <>
+          <ChatWindowDesktopHeader />
+          <MessageContainer messages={messages} />
+          <Footer addMessage={addMessage} />
+        </>
+      ) : (
+        <EmptyChat />
+      )}
     </div>
   );
 };
