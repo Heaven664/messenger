0495b7457b2b004c0a56334e5e9f815c60119a4f
Omar Hamid <omarhamid664@gmail.com>
2024-01-13 10:57:01 -0700

    Show MobileChatWindow only when there is ChatWindowContext

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import ChatWindowDesktop from "../ChatWindowDesktop/ChatWindowDesktop";
 import ChatWindowMobile from "../ChatWindowMobile/ChatWindowMobile";
 import NavbarDesktop from "../Navbar/NavbarDesktop";
 import styles from "./Layout.module.scss";
 import { ComponentProps } from "@/types/Layout/types";
+import ChatWindowContext from "@/context/ChatWindowContext";
 
 const Layout = ({ children }: ComponentProps) => {
+  const chatWindowDesktopContext = useContext(ChatWindowContext);
+  const chatWindowSelected = chatWindowDesktopContext?.headerInfo !== null;
+
   return (
     <div className={styles.layoutContainer}>
       <NavbarDesktop />
@@ -12,9 +17,13 @@ const Layout = ({ children }: ComponentProps) => {
       <div className={styles.chatWindowDesktop}>
         <ChatWindowDesktop />
       </div>
-      <div className={styles.chatWindowMobile}>
-        <ChatWindowMobile />
-      </div>
+      {chatWindowSelected ? (
+        <div className={styles.chatWindowMobile}>
+          <ChatWindowMobile />
+        </div>
+      ) : (
+        <div className={styles.hidden}></div>
+      )}
     </div>
   );
 };
