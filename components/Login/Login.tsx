bc015a4280c264f1ae9d488ee5c981f006b07dea
Omar Hamid <omarhamid664@gmail.com>
2024-01-14 15:20:20 -0700

    Create SignIn header

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,7 +1,19 @@
 import styles from "./Login.module.scss";
 
 const Login = () => {
-  return <div>Log In</div>;
+  return (
+    <div className={styles.layout}> 
+      <div className={styles.container}>
+        <div className={styles.loginWindow}>
+          <div className={styles.formHeaderSection}>
+            <h2>Welcome Back </h2>
+            <p>Sign in to continue to Messenger</p>
+          </div>
+          <div className={styles.formInputsSection}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
