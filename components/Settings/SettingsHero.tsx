48fe13ab62a6a575fb307f990b2ddbbb2b705c22
Omar Hamid <omarhamid664@gmail.com>
2024-02-27 19:55:25 -0700

    Update settings profile image url

--- components/Settings/SettingsHero.tsx
+++ components/Settings/SettingsHero.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import styles from "@/components/Settings/SettingsHero.module.scss";
 import CameraAltRounded from "@mui/icons-material/CameraAltRounded";
+import { useSession } from "next-auth/react";
 
 const SettingsHero = () => {
+  const session = useSession()?.data;
   return (
     <div className={styles.layout}>
       <div className={styles.container}>
@@ -10,15 +12,15 @@ const SettingsHero = () => {
           <div className={styles.imageContainer}>
             <div className={styles.imageBackground}>
               <Image
-                src={"/general/main.HEIC"}
+                src={session!.user!.imageSrc}
                 width={150}
                 height={150}
                 alt="hero-image"
               />
             </div>
-              <div className={styles.changeIconContainer}>
-                <CameraAltRounded className={styles.changeIcon} />
-              </div>
+            <div className={styles.changeIconContainer}>
+              <CameraAltRounded className={styles.changeIcon} />
+            </div>
           </div>
           <p>Omar Hamid</p>
         </div>
