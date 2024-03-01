eddb56fe7f8495ab188727969e4668b92e128235
Omar Hamid <omarhamid664@gmail.com>
2024-03-01 16:58:19 -0700

    Edit contact form

--- components/General/AddContactForm.tsx
+++ components/General/AddContactForm.tsx
@@ -1,18 +1,25 @@
 import { TextField } from "@mui/material";
 import styles from "./AddContactForm.module.scss";
 import { useRef } from "react";
+import { useSession } from "next-auth/react";
 
 const AddContactForm = () => {
+  const session = useSession()?.data;
   const emailRef = useRef<HTMLInputElement>(null);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(emailRef.current?.value.trim());
+    const data = {
+      id: session?.user.id,
+      friendEmail: emailRef.current?.value.trim(),
+    };
+    console.log(data);
   };
   return (
     <form className={styles.container} onSubmit={handleSubmit}>
       <div className={styles.inputSection}>
         <TextField
           variant="outlined"
+          type="email"
           label={"Email"}
           placeholder="Enter Contact Email"
           className={styles.inputField}
