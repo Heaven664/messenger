fc0b13cb2feac5e93f3ca4bb335c571eb5034383
Omar Hamid <omarhamid664@gmail.com>
2024-03-09 14:53:02 -0700

    Fix unique key error in MessageItem when messages is empty array

--- components/Message/MessageContainer.tsx
+++ components/Message/MessageContainer.tsx
@@ -1,25 +1,21 @@
 import { MessageType } from "@/types/ChatWindow/types";
 import styles from "./MessageContainer.module.scss";
 import MessageItem from "./MessageItem";
-import { dummyMessages } from "@/dummyMessage";
 
 type P = {
   messages: MessageType[];
 };
-// TODO
-// interface IMessageContainerProps {
-// messages: MessageType[];
-// }
 
 const MessageContainer = ({ messages }: P) => {
   const allMessages = messages.map((message, index) => {
     let lastMessage = true;
+    // Check if the next message is from the same sender
     if (index !== messages.length - 1) {
       lastMessage = messages[index + 1].senderEmail !== message.senderEmail;
     }
     return (
       <MessageItem
-        key={message._id}
+        key={message._id || index}
         message={message}
         lastMessage={lastMessage}
       />
