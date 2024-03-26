c25617bdec785cfebb005bc207ac0d6805733b6c
Omar Hamid <omarhamid664@gmail.com>
2024-03-25 23:00:54 -0600

    Use compressorjs instead of canvas for image compression

--- helpers/General/index.tsx
+++ helpers/General/index.tsx
@@ -44,25 +44,3 @@ export const findChatByName = (chats: ChatType[], inputValue: string) => {
   return filteredChats;
 };
 
-export const dataURItoBlob = (dataURI: string) => {
-  // convert base64 to raw binary data held in a string
-  var byteString = atob(dataURI.split(",")[1]);
-
-  // separate out the mime component
-  var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
-
-  // write the bytes of the string to an ArrayBuffer
-  var ab = new ArrayBuffer(byteString.length);
-
-  // create a view into the buffer
-  var ia = new Uint8Array(ab);
-
-  // set the bytes of the buffer to the correct values
-  for (let i = 0; i < byteString.length; i++) {
-    ia[i] = byteString.charCodeAt(i);
-  }
-
-  // write the ArrayBuffer to a blob, and you're done
-  let blob = new Blob([ab], { type: mimeString });
-  return blob;
-};
